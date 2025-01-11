88c406d56aafada7d239c37ff9387e0500456b5e
Gaurav Gupta <gaurav.mitmpl2022@learner.manipal.edu>
2025-01-12 04:02:59 +0530

    Fixed the home page and the footer to stick at the bottom of the window

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className=" flex-grow-0 flex-shrink-0 bg-gray-100 text-center py-4 mt-40">
+    <footer className=" footer bg-gray-100 text-center py-4 ">
       <p className="text-gray-600">© 2024 MIT Research Spotlight</p>
       <div className="flex justify-center mt-4">
         <Link to="/about" className="text-blue-600 font-medium">
