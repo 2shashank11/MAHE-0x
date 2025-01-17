2c5ecf335659dc25a02ba0b7c429c9d3b98d455c
Shashank <2shashank11@gmail.com>
2025-01-17 10:25:17 +0530

    Revert "updated the Homepage look, configured the buttons"
    This reverts commit 8a5e56b41658313050d3f95ae17e45e1c236463f.
    
    reverted back changes

--- client/src/components/features.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Features() {
-  return (
-    <div className=" text-center mt-36 pt-40 h-96 w-full bg-gray-100">
-      Feature Section
-    </div>
-  );
-}
--- client/src/components/future_scope.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FutureScope() {
-  return (
-    <div className="text-center pt-40 h-96 w-full mt-40 bg-gray-100">
-      Full Scope
-    </div>
-  );
-}
