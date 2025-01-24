f2afcf5e56f03835439e875664c6c34b47f31309
Gaurav Gupta <gaurav.mitmpl2022@learner.manipal.edu>
2025-01-24 19:01:14 +0530

    fixed the issue of faulty alignment in forms and everywhere

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -15,12 +15,12 @@ export function HomeDescription({ onTabchange }) {
   return (
     <div className="text-center  bg-gray-100  rounded-lg br px-4 md:px-8 lg:px-24 pb-2">
       <div className="justify-items-center pt-40 ">
-        <p className="text-xs py-2 px-10 bg-gray-200 rounded-md">
+        <p className="text-xs py-2 px-6 bg-gray-200 rounded-md">
           Used by more than 1500+ faculties
         </p>
       </div>
 
-      <div className="pt-12">
+      <div className="pt-6">
         <h1 className="text-3xl sm:text-4xl md:text-6xl font-semibold tracking-normal ">
           Bringing<i>organization</i>to the
           <br />
@@ -39,18 +39,18 @@ export function HomeDescription({ onTabchange }) {
               <div className="flex justify-around mt-8">
                 <Link to="/signup">
                   <Button
-                    className="bg-gradient-to-r from-stone-700 to-black text-white font-serif text-lg px-16 py-7 rounded-lg shadow-xl shadow-gray-700/40"
+                    className="bg-gradient-to-r from-stone-700 to-black text-white font-serif text-medium px-10 py-5 rounded-md shadow-lg shadow-gray-700/40"
                     name="signupButton"
-                    size="lg"
+                    size="md"
                   >
                     Sign up
                   </Button>
                 </Link>
                 <Link to="/all-achievements">
                   <Button
-                    className="bg-gradient-to-r from-gray-300 to-gray-400 text-white font-serif text-lg px-14 py-7 rounded-lg shadow-xl shadow-gray-600/40"
+                    className="bg-gradient-to-r from-gray-300 to-gray-400 text-white font-serif text-medium px-10 py-5 rounded-md shadow-lg shadow-gray-600/40"
                     name="dashboard"
-                    size="lg"
+                    size="md"
                   >
                     Dashboard
                   </Button>
--- client/src/components/newNav.jsx
+++ client/src/components/newNav.jsx
@@ -5,20 +5,31 @@ export default function Navigation() {
   return (
     <header className="flex justify-items-center justify-between py-2 mt-6">
       <div className="flex items-center -space-x-1 text-xl font-serif">
-        <div className="w-14 h-14 bg-black rounded-sm flex items-center justify-center text-xl text-white font-serif">
+        <div className="w-12 h-12 bg-black rounded-sm flex items-center justify-center text-l text-white font-serif">
           R
         </div>
-        <div className="text-xl font-serif text-black">Research Spotlight</div>
+        <div className="text-medium font-serif text-black">
+          Research Spotlight
+        </div>
       </div>
 
-      <nav className="space-x-24 border-1 border-gray-100 px-10 py-4 rounded-md">
-        <Link href="/research" className="text-gray-600 hover:text-gray-900">
+      <nav className="space-x-20 border-1 border-gray-100 px-8 py-3 rounded-md">
+        <Link
+          href="/research"
+          className="text-xs text-gray-600 hover:text-gray-900"
+        >
           Research
         </Link>
-        <Link href="/contact" className="text-gray-600 hover:text-gray-900">
+        <Link
+          href="/contact"
+          className="text-xs text-gray-600 hover:text-gray-900"
+        >
           Contact
         </Link>
-        <Link href="/about" className="text-gray-600 hover:text-gray-900 px-8">
+        <Link
+          href="/about"
+          className="text-xs text-gray-600 hover:text-gray-900 px-8"
+        >
           About
         </Link>
       </nav>
@@ -26,9 +37,9 @@ export default function Navigation() {
       <div>
         <Link to="/signin">
           <Button
-            className="bg-gray-200 hover:bg-black text-white font-serif text-lg px-14 py-7 rounded-lg"
+            className="bg-gray-200 hover:bg-black text-white font-serif text-l px-8 py-4 rounded-sm "
             name="loginButton"
-            size="lg"
+            size="md"
           >
             Sign in
           </Button>
